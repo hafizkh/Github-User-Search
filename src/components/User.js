2d89cdf6671a5d5a57a96624d108b079230afeff
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-07-16 15:11:29 +0300

    Small Changes in user card

--- src/components/User.js
+++ src/components/User.js
@@ -10,10 +10,10 @@ const User = ({ user }) => {
       <Card style={{ borderRadius:'3rem' , width: '21rem' , height: '20rem', left: '38%', top: '8px'}}>
   <Card.Img style={{borderRadius:'3rem'}} variant="center" src= {user.avatar_url} alt = "User Image file" />
   <Card.Body>
-    <Card.Text><span>Name of User: </span>{user.name}</Card.Text>
-    <Card.Text><span>Followers of {user.name}: </span>{user.followers}</Card.Text>
-    <Card.Text><span>Name of Company: </span>{user.company}</Card.Text>
-    <Card.Text><span>No. of Repos: </span>{user.public_repos}</Card.Text>
+    <Card.Text><span><strong> Name of User:</strong> </span>{user.name}</Card.Text>
+    <Card.Text><span><strong> Followers of {user.name}: </strong> </span>{user.followers}</Card.Text>
+    <Card.Text><span><strong> Name of Company:</strong> </span>{user.company}</Card.Text>
+    <Card.Text><span><strong> No. of Repos: </strong></span>{user.public_repos}</Card.Text>
       
   </Card.Body>
 </Card>
