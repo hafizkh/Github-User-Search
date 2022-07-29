70129b54c10578149087082f6663c9af998e1b36
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-07-29 04:03:47 +0300

    Card border

--- src/components/User.js
+++ src/components/User.js
@@ -7,9 +7,9 @@ import { Card } from 'react-bootstrap'
 const User = ({ user }) => {
   return (
     <div >
-      <Card className='mt-3'>
+      <Card className='mt-3' style={{border: '1px solid'}}>
         <Card.Img style={{ width: 'auto', height: 'auto' }} src={user.avatar_url} alt="User Image file" />
-        <Card.Body style={{border: '1px solid'}}>
+        <Card.Body>
           <Card.Text style={{fontSize: 'x-large'}}><span><strong> Name of User:</strong> </span>{user.name}</Card.Text>
           <Card.Text style={{fontSize: 'x-large'}}><span><strong> Followers of {user.name}: </strong> </span>{user.followers}</Card.Text>
           <Card.Text style={{fontSize: 'x-large'}}><span><strong> Name of Company:</strong> </span>{user.company}</Card.Text>
