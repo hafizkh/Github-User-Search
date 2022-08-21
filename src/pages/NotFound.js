34bddba48bcf017918a13a5435a031e3174b0498
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-08-22 01:37:05 +0300

    Not Found page styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import "../App.css"
+import { useNavigate } from 'react-router-dom'
+import notFound from '../images/404_Not_found.jpg'
 
 const NotFound = () => {
+  const navigate = useNavigate()
+
   return (
-    <div className='App'>
-      <h1>Error 404</h1>
-      <p>Page Not Found</p>
+    <div className='container mt-4'
+    style={{marginLeft:'28%'}}
+    >
+    <img src= {notFound} alt="Not Found" />
+    <div className="card-body">
+      <button type='button' 
+      className="btn btn-primary btn-rounded" 
+      style={{marginLeft:'30%'}}
+      onClick={()=>navigate("/home")}>Home</button>
     </div>
+  </div>
   )
 }
 
